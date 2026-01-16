a78fdc3f86496fa7a545fe885cc939187b5a0990
NuwaniSudarshika <nuwanisudarshi@gmail.com>
2026-01-16 20:19:52 +0530

    correct command

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const User = require("../models/User.model"); // make sure this path is correct
 require("dotenv").config();
